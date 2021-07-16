f0bd668ccd8a8487f99abb32e56b7bb0455897ae
Cata312514 <atacatherine@cityuniversity.edu>
2021-07-16 14:55:44 -0600

    Added new TestCase1

---
+++ Submission1/TestCase1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/* Test Case 1 for Submission1
+Simple script to introduce self.
+Asking user name input and a simple greeting.
+*/
+
+namespace MyBiography
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Catherine Ata";
+            int Age = 39;
+            bool isFemale = true;
+            float numberTerms = 4.5f;
+
+            Console.WriteLine("My name is {0} .", fullName);
+            Console.WriteLine("I am {0} years old.", Age);
+            Console.WriteLine("Female: {0}", isFemale);
+            Console.WriteLine("Student at CityU for {0} terms.", numberTerms);
+
+        }
+    }
+}

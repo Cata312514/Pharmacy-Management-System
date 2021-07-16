94f6a71d266dbdcaf217550a90250b5e265a03f9
Cath-2A <87244339+Cath-2A@users.noreply.github.com>
2021-07-16 10:13:24 -0600

    combined 2 string variables into 1

--- Submission1/code1.cs
+++ Submission1/code1.cs
@@ -9,16 +9,18 @@ namespace MyBiography
     {
         static void Main(string[] args)
         {
-            string firstName = "Catherine";
-            string lastName = "Ata";
+            string fullName = "Catherine Ata";
             int Age = 39;
             bool isFemale = true;
             float numberTerms = 4.5f;
 
-            Console.WriteLine("My name is {0} {1}.", firstName, lastName);
+            Console.WriteLine("My name is " + fullName);
             Console.WriteLine("I am {0} years old.", Age);
             Console.WriteLine("Female: {0}", isFemale);
             Console.WriteLine("Student at CityU for {0} terms.", numberTerms);
+            
+            // Add user input:
+            
         }
     }
 }
